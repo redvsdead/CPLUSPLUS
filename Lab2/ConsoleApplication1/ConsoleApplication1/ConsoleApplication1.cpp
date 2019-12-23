bc96eb286fe459860c845f3695858aa304f3f4eb
descent.teich@gmail.com <descent.teich@gmail.com>
2019-12-23 10:26:25 +0300

    добавлена сумма

--- Lab2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Lab2/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -8,7 +8,7 @@ using namespace std;
 //проверка строки на то, является ли она числом
 string EnterNum(int i)
 {
-	std::cout << "Введите " << i << "-е число" << endl;
+	cout << "Введите " << i << "-е число" << endl;
 	string num;
 	bool ok;
 	bool again = true;
@@ -25,7 +25,7 @@ string EnterNum(int i)
 		again = !ok;
 		if (again)
 		{
-			std::cout << "Введенная строка не является числом. Попробуйте снова." << endl;
+			cout << "Введенная строка не является числом. Попробуйте снова." << endl;
 		}
 	}
 	return num;
@@ -35,74 +35,25 @@ string EnterNum(int i)
 
 int main()
 {
-
-
-
 	setlocale(LC_ALL, "Russian");
 	LongLong one, two, answer;
 
-	one.SetNum("1");
-	assert(one.low == 1);
-	assert(one.high == 0);
-
-
-	one.SetNum("-1");
-	assert(one.low == 0xFFFFFFFF);
-	assert(one.high == 0xFFFFFFFF);
-
-
-
-	one.SetNum("-2"); 
-	assert(one.low == 0xFFFFFFFE);
-	assert(one.high == 0xFFFFFFFF);
-
-
-	one.SetNum("+???100000000????");
-	assert(one.low == 0);
-	assert(one.high == 1);
-
-
-	one.SetNum("-??????1");
-	assert(one.low == 0xFFFFFFFF);
-	assert(one.high == 0xFFFFFFFF);
-
-
-
-	one.SetNum("-??????2");
-	assert(one.low == 0xFFFFFFFE);
-	assert(one.high == 0xFFFFFFFF);
-
 	one.SetNum(EnterNum(1));
 	two.SetNum(EnterNum(2));
-	std::cout << std::hex << "\n";
 	one.Print();
 	two.Print();
 
 	bool ok = answer.Equality(one, two);
 
 	answer.Addition(one, two);
-	std::cout << "Сумма: ";
+	cout << "Сумма: ";
 	answer.Print();
 
-	/*if (!ok)
-	{
-		answer.Subtraction(two, one);
-		std::cout << "Разность: ";
-		std::cout << "-";
-		answer.Print();
-	}
-	else
-	{*/
 	answer.Subtraction(one, two);
-	std::cout << "Разность: ";
+	cout << "Разность: ";
 	answer.Print();
-	//}
+
 	answer.Multiplication(one, two);
-	std::cout << "Произведение: ";
+	cout << "Произведение: ";
 	answer.Print();
-	/*
-	answer.Division(one, two);
-	std::cout << "Частное: ";
-	answer.Print();
-	*/
 }
--- Lab2/ConsoleApplication1/ConsoleApplication1/LongLong.cpp
+++ Lab2/ConsoleApplication1/ConsoleApplication1/LongLong.cpp
@@ -5,13 +5,8 @@ using namespace std;
 
 void LongLong::SetNum(string num)
 {				
-	high = static_cast<int32_t> (((atoll(num.c_str())) & 0xFFFFFFFF00000000LL) >> 32);
-	low = static_cast<uint32_t>((atoll(num.c_str())) & 0xFFFFFFFFLL);
-	if ((atoll(num.c_str()) < 0) && ((-1)*(atoll(num.c_str()) <= 0xFFFFFFFFLL)))
-	{
-		low--;
-	}
-	//std::cout << 0xFFFFFFFF - 5 << endl << low << endl;
+	high = (int32_t)(((atoll(num.c_str())) & 0xFFFFFFFF00000000LL) >> 32);
+	low = (uint32_t)((atoll(num.c_str())) & 0xFFFFFFFFLL);
 }
 
 int32_t LongLong::GetHigh()
@@ -36,19 +31,9 @@ void LongLong::SetLow(uint32_t n)
 
 void LongLong::Print()
 {
-	long long n;
-	/*if (high == 0xFFFFFFFF)
-	{
-		n = 0xFFFFFFFF + low + 1;
-		n = n * (-1);
-	}  
-	else*/
-	{
-		n = (static_cast<long long>(high) << 32) | low;
-	}
-	if (n < 0)
-		n++;
-	std::cout << n << "\n";
+	long long m;
+	m = (static_cast<long long>(high) << 32) | low;
+	cout << m << endl;
 }
 
 ////////////////////////
@@ -58,31 +43,31 @@ bool LongLong::Equality(class LongLong a, class LongLong b) 				//сравнение
 	bool ok = (a.high == b.high) && (a.low == b.low);
 	if (ok)
 	{
-		std::cout << "Числа равны" << "\n";
+		cout << "Числа равны" << "\n";
 		return true;
 	}
 	else
 	{
 		if (a.high > b.high)
 		{
-			std::cout << "Первое число больше" << endl;
+			cout << "Первое число больше" << endl;
 			return true;
 		}
 		else
 		{
 			if (a.high < b.high)
 			{
-				std::cout << "Первое число меньше" << endl;
+				cout << "Первое число меньше" << endl;
 				return false;
 			}
 			else
 				if (a.low > b.low)
 				{
-					std::cout << "Первое число больше" << endl;
+					cout << "Первое число больше" << endl;
 					return true;
 				}
 				else
-					std::cout << "Первое число меньше" << endl;
+					cout << "Первое число меньше" << endl;
 			return false;
 		}
 	}
@@ -90,7 +75,7 @@ bool LongLong::Equality(class LongLong a, class LongLong b) 				//сравнение
 
 void LongLong::Multiplication(class LongLong a, class LongLong b)						//произведение
 {
-	long long overflow;
+	/*long long overflow;
 	if (!(a.high == 0xFFFFFFFF && b.high == 0xFFFFFFFF)) 
 	{
 
@@ -108,7 +93,7 @@ void LongLong::Multiplication(class LongLong a, class LongLong b)						//произве
 	{
 		high = ((a.low * b.low) & 0xFFFFFFFF00000000LL) >> 32;
 		low = (a.low * b.low) & 0xFFFFFFFFLL;
-	}
+	}*/
 }
 
 void LongLong::Division(class LongLong a, class LongLong b)							//частное
@@ -118,112 +103,16 @@ void LongLong::Division(class LongLong a, class LongLong b)							//частное
 
 void LongLong::Addition(class LongLong a, class LongLong b)							//сумма
 {
-	/*high = 0;
-		bool overflow = (0xFFFFFFFFLL - a.low) <= b.low;
-		if (overflow)
-		{
-			low = a.low - 0xFFFFFFFFLL + 1 + b.low;
-			high++;
-		}
-		else
-		{
-			low = a.low + b.low;
-		}
-		high = a.high + b.high;
-		if (a.high == 0xFFFFFFFF)
-			low = low - 2;
-		if (b.high == 0xFFFFFFFF)
-			low = low - 2;
-	else
-	{
-		high = 0xFFFFFFFF;
-		low = a.low + b.low;
-	}*/
+	bool overf = 0xFFFFFFFFLL - a.low + 1 <= b.low;
+	low = (a.low + b.low) & 0xFFFFFFFFLL;
 	high = a.high + b.high;
-	if (!((a.high >= 0) ^ (b.high >= 0)))
+	if (overf) 
 	{
-		bool overflow = (0xFFFFFFFFLL - a.low) <= b.low;
-		if (overflow)
-		{
-			low = a.low - 0xFFFFFFFFLL + 1 + b.low;
-			high++;
-		}
-		else
-		{
-			low = a.low + b.low;
-		}
-		high = a.high + b.high;
-		if (a.high == 0xFFFFFFFF)
-			low = low - 2;
-	}
-	else
-		low = a.low + b.low;
+		high++; 
+	};
 }
 
 void LongLong::Subtraction(class LongLong a, class LongLong b)			//разность
 {
-	/*//if (!(a.high == 0xFFFFFFFF && b.high == 0xFFFFFFFF))
-	{
-		high = a.high - b.high;
-		if (a.low < b.low)
-		{
-
-			low = a.low << 32 - b.low;
-			a.high >> 32;
-		}
-		else
-		{
-			low = a.low - b.low;
-		}
-	}
-	/*else
-	{
-		if (a.low < b.low)
-		{
-			high = 0;
-			low = b.low - a.low;
-		}
-		else
-		{
-			high = 0xFFFFFFFF;
-			low = a.low - b.low;
-		}
-	}*/
-
-	if (!((a.high >= 0) ^ (b.high >= 0)))
-	{
-		high = a.high + b.high;
-		bool overflow = (0xFFFFFFFFLL - a.low) <= b.low;
-		if (overflow)
-		{
-			low = a.low - 0xFFFFFFFFLL + 1 + b.low;
-			high++;
-		}
-		else
-		{
-			low = a.low - b.low;
-		}
-		high = a.high + b.high;
-		if (a.high == 0xFFFFFFFF)
-			low = low - 2;
-		if (b.high == 0xFFFFFFFF)
-			low = low - 2;
-	}
-	else
-	{
-		{
-			high = a.high - b.high;
-			if (a.low < b.low)
-			{
-
-				low = a.low << 32 - b.low;
-				a.high >> 32;
-			}
-			else
-			{
-				low = a.low - b.low;
-			}
-		}
-	}
-
+	
 }
